115465acd5ccbcbd52f21ac8305b4313829d77e5
wangjicong <wangjicong6403660@126.com>
2017-05-25 19:36:29 +0800

    modify

--- app/src/main/java/wjc/seekbar/MainActivity.java
+++ app/src/main/java/wjc/seekbar/MainActivity.java
@@ -1,13 +1,42 @@
 package wjc.seekbar;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RemoteViews;
+import android.widget.TextClock;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity  {
+
+    private Activity mContext;
+    private LayoutInflater mLayoutInflater;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
